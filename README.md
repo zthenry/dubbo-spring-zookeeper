66cc3a92552107ff9b03061d99c1487f355c768f
henry <zhangtao111111@126.com>
2014-10-20 17:18:42 +0800

    启动服务提供者 之二 com.alibaba.dubbo.container.Main

--- README.md
+++ README.md
@@ -1,1 +1,5 @@
 dubbo整合spring,zookeeper,实现接口的远程调用demo
+
+//打包命令
+mvn dependency:copy-dependencies -DoutputDirectory=target package
+
---
+++ service-test-provider2/src/main/java/com/henry/service/impl/HelloServiceImpl.java
@@ -0,0 +1,33 @@
+/*
+ * 文 件 名:  HelloServiceImpl.java
+ * 版    权:  Huawei Technologies Co., Ltd. Copyright YYYY-YYYY,  All rights reserved
+ * 描    述:  <描述>
+ * 修 改 人:  root
+ * 修改时间:  2014-10-19
+ * 跟踪单号:  <跟踪单号>
+ * 修改单号:  <修改单号>
+ * 修改内容:  <修改内容>
+ */
+package com.henry.service.impl;
+
+import com.henry.service.HelloService;
+
+/**
+ * <一句话功能简述>
+ * <功能详细描述>
+ * 
+ * @author  root
+ * @version  [版本号, 2014-10-19]
+ * @see  [相关类/方法]
+ * @since  [产品/模块版本]
+ */
+public class HelloServiceImpl implements HelloService
+{
+
+    public String sayHello(String name)
+    {
+        System.out.println("hello:"+name);
+        return name;
+    }
+    
+}
